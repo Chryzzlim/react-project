fa5e14f7fd0674348a9754b81513f088a01a6cc6
Chayliverse <chryzzmichael.policarpio.l@bulsu.edu.ph>
2022-04-12 22:25:08 +0800

    login

---
+++ src/Signin/Form.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import './Form.css';
+import LoginForm from './LoginForm';
+import FormSuccess from './FormSuccess';
+import Footer from '../components/Footer';
+const Form = () => {
+    const [isSubmitted, setIsSubmitted] = useState(false);
+  
+    function submitForm() {
+      setIsSubmitted(true);
+    }
+    return (
+      <>
+        <div className='form-container'>
+          {!isSubmitted ? (
+            <LoginForm submitForm={submitForm} />
+          ) : (
+            <FormSuccess />
+          )}
+        </div>
+        <Footer/>
+      </>
+    );
+  };
+  
+  export default Form;
